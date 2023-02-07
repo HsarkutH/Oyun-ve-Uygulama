ada1f7f17d6b3d6e46eb715738bcece01b938da3
HsarkutH <sarkut.huseyin@gmail.com>
2023-02-07 16:34:55 +0300

    upload 07.02

---
+++ lintsornek/android/app/src/main/kotlin/com/example/lintsornek/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lintsornek
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lintsornek/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  final i = 5;
+  print(i);
+  print('merhaba!');
+}
